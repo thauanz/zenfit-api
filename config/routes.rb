f54f210c6a671346f788b2248b22d885a6642e82
Thauan Zatta <thauanz@gmail.com>
2018-03-15 15:19:02 +1000

    Add endpoint to request report per week

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :zentimes
     resources :users
+    get 'report' => 'reports#index'
   end
 
   devise_for :users,
