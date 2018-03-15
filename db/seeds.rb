edd0693185f992cea9a1390460c30f5583022b55
Thauan Zatta <thauanz@gmail.com>
2018-03-15 14:30:26 +1000

    Add users to start project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 User.create!(name: 'Yoda', email: 'yoda@zenfit.com', password: '123456789', role: 'admin')
 User.create!(name: 'Anakin', email: 'anakin@zenfit.com', password: '123456789', role: 'manager')
 
-user = User.create!(name: 'Luke', email: 'luke@zenfit.com', password: '123456789', role: 'regular')
+luke = User.create!(name: 'Luke', email: 'luke@zenfit.com', password: '123456789', role: 'regular')
+r2 = User.create!(name: 'R2', email: 'r2@zenfit.com', password: '123456789', role: 'regular')
 
-first_day = Date.today - 10.day
-(first_day..Date.today).each do |d|
-  Zentime.create(date_record: d, time_record: rand(1..200), user: user)
+def create_zentimes(user)
+  first_day = Date.today - 4.weeks
+  (first_day..Date.today).each do |d|
+    Zentime.create(date_record: d, time_record: rand(1..200), user: user)
+  end
 end
+
+create_zentimes(luke)
+create_zentimes(r2)
